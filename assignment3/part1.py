f53f9fb72b45e4bc87528908e056ff0d1ee683b0
Eyal Oren <oreneya@gmail.com>
2019-04-29 20:26:57 +0300

    Add files via upload

--- assignment3/part1.py
+++ assignment3/part1.py
@@ -6,9 +6,11 @@ inp = np.array([[0,0],[0,1],[1,0],[1,1]])
 tar = np.array([[0],[1],[1],[0]])
 
 # create & train an instance of 'XOR' neural network
-pXOR = Backprop(2,1, 3)
+pXOR = Backprop(2,1, 30)
 pXOR.train(inp, tar, niter=10000, eta=10.)
 
 # print 'XOR' results
 print("\nTesting 'XOR' backpropagation NN:")
-[print("pattern: " + str(i) + " | result: " + str(pXOR.test(i))) for i in inp]
+#[print("pattern: " + str(i) + " | result: " + str(pXOR.test(i))) for i in inp]
+for i in inp:
+	print("pattern: " + str(i) + " | result: " + str(pXOR.test(i)))
