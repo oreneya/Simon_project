Fixed backprop RMS error reporting

assignment6/backprop.py
@@ -56,7 +56,7 @@ class Backprop:
                 O_net = np.dot(H, self.who)
                 O = self.squash(O_net)
 
-                O_err = (T[j]-O) 
+                O_err = T[j] - O 
                 O_del = O_err * self.dsquash(O_net)
 
                 # Sum the squared error over the output units

assignment6/part1.py
@@ -19,10 +19,7 @@ def shift(x):
 # create & train an instance of 'XOR' neural network
 bp = Backprop(1, 1, 20)
 inputs = xorseq(1000)
-#targets = shift(inputs)
-
-inputs = np.random.randint(0, 2, (1000,1))
-targets = np.random.randint(0, 2, (1000,1)) 
+targets = shift(inputs)
 
 bp.train(inputs, targets, niter=100, eta=0.5, report=10)
 