58c8d20b96e3c41e16abbbfae117e4fb44c7f15a
Eyal Oren <oreneya@gmail.com>
2019-06-20 23:48:54 +0300

    SRN xor rms still high

--- assignment6/part1.py
+++ assignment6/part1.py
@@ -17,7 +17,11 @@ def shift(x):
         shifted = np.append(x[1:], 0)
         return shifted.reshape(len(x), 1)
 
-print('Training feed-forward net')
+def xorerr(pred, targ):
+	err = pred - targ
+	return err**2
+
+print('\nTraining feed-forward net')
 bp = Backprop(1, 1, 20)
 inputs = xorseq(1000)
 targets = shift(inputs)
@@ -26,3 +30,21 @@ bp.train(inputs, targets, niter=100, eta=0.5, report=10)
 print('\nTraining SRN')
 srn = SRN(1, 1, 20)
 srn.train(inputs, targets, niter=100, eta=0.5, report=10)
+
+# evaluating bp and srn on xor problem
+
+bp_sumerr2 = 0
+srn_sumerr2 = 0
+
+for i, t in zip(inputs[1::3], targets[1::3]):
+
+	bp_xor_pred = bp.test(i)
+	bp_sumerr2 += xorerr(bp_xor_pred, t)
+
+	srn_xor_pred = srn.test(i)
+	srn_sumerr2 += xorerr(srn_xor_pred, t)
+
+bp_xor_rms = np.sqrt(bp_sumerr2 / (len(inputs)/3))
+srn_xor_rms = np.sqrt(srn_sumerr2 / (len(inputs)/3))
+
+print("\nXOR Error:   BP = {}   SRN = {}".format(bp_xor_rms, srn_xor_rms))
